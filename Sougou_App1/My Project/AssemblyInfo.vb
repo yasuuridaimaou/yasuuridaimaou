1d7e86e95a1df76a1bf46747554351886feaf977
Koujirou Murayama <yasuuridaimaou@gmail.com>
2024-10-26 10:17:38 +0900

    最初

---
+++ Sougou_App1/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下を通して制御されます
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("Sougou_App1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Sougou_App1")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("7dc5d6e7-fef2-44bb-bd6b-5de25b02717c")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
